efa24ac3b775bf5d20b44ba0a573e544ae4b9266
said124 <165512725+said124@users.noreply.github.com>
2025-11-03 09:00:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let user = {
+    name: prompt('Ismingizni kiriting') ,
+    ID: prompt('Space ID kiriting') ,
+    password: prompt('Space parol kiriting')
+}
+
+console.log(user);
+
+
+// alert('Щас вы будете вводить свои данные')
+
+// let danniye = {
+//     name: prompt('Введите имя') ,
+//     age: prompt('Укажите возвраст') ,
+//     klass: prompt('Укажите школьный класс') ,
+//     adress: prompt('Укажите адресс') ,
+//     number: prompt('Введите номер телефона')
+// }
+
+// console.log(danniye);
